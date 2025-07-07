da6fdfd0e1f6ae32bc7164366e82e60bbabb233e
harshitdncy <harshit@dcny.co>
2025-07-07 15:21:26 +0530

    ADD: comment

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,5 @@
+//Exported hooks
+
 export { useWindowSize } from './hooks/useWindowSize';
 export { useDebounce } from './hooks/useDebounce';
 export { useLocalStorage } from './hooks/useLocalStorage';
