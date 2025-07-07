acc0d41019ea46fb8cad303307cb38498bd8df2f
harshitdncy <harshit@dcny.co>
2025-07-07 15:24:26 +0530

    ADD: comment

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,12 @@
 //Exported hooks
+// a hook to get the window size
+// a hook to debounce a value
+// a hook to get a value from local storage
+
+
+
+
+
 
 export { useWindowSize } from './hooks/useWindowSize';
 export { useDebounce } from './hooks/useDebounce';
